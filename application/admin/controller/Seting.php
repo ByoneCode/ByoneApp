e78daaec3ee4ee54a3573b35d82883c2805b14e6
byone <2488068072@qq.com>
2020-04-03 15:09:22 +0800

    Please enter the commit message for your changes. Lines starting 后台设置功能添加

--- application/admin/controller/Seting.php
+++ application/admin/controller/Seting.php
@@ -1,7 +1,6 @@
 <?php
 namespace app\admin\controller;
 use app\admin\controller\Byse;
-use \think\Session;
 use \think\Request;
 use \think\Db;
 class Seting extends Byse
@@ -11,8 +10,21 @@ class Seting extends Byse
        $res = Db::name('seting')
        ->find();
        $this->assign('info',$res);
-       dump(Session::get());
        return $this->fetch();
+    }
+
+    public function setData(){
+        $data =  input('param.');
+        
+        $res = Db::name('seting')
+        ->where('id',1)
+        ->update($data);
+
+        if($res == 1){
+            return ['code'=>1,'info'=>'设置成功'];
+        }else{
+            return ['code'=>0,'info'=>'设置错误'];
+        }
     }
    
 }
